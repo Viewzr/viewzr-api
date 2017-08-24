eb4ee6ad91213e15baaaf2752a668781ac7298a7
Scott Clampet <scottclampet@Scotts-MacBook-Pro.local>
2017-08-24 12:53:54 -0600

    initial festival model and controller setup

---
+++ api/lib/controllers/FestivalController.js
@@ -0,0 +1,27 @@
+var Festival = require(../models/Festival);
+
+module.exports = {
+  find: function() {
+
+  },
+
+  findById: function() {
+
+  },
+
+  create: function() {
+
+  },
+
+  remove: function() {
+
+  },
+
+  update: function() {
+
+  },
+
+  destroy: function() {
+
+  }
+}
